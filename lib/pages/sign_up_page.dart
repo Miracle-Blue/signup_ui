8ee3fa3d14cbf89b365e258f7b59aed95312bea1
Miracle-Blue <merlin.com8.ar@gmail.com>
2022-01-17 22:51:36 +0500

    SignUp UI

--- lib/pages/sign_up_page.dart
+++ lib/pages/sign_up_page.dart
@@ -27,197 +27,200 @@ class _SignUpPageState extends State<SignUpPage> {
                 Colors.grey.shade400,
               ]),
         ),
-        child: Column(
-          children: [
-            /// #header
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40),
-                child: Align(
-                  alignment: Alignment.centerRight,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// #header
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: const [
+                        Text(
+                          "Sign Up",
+                          style: TextStyle(color: Colors.white, fontSize: 40),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Welcome",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              /// #body
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                child: Container(
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: const [
-                      Text(
-                        "Sign Up",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
+                    children: [
+                      const SizedBox(
+                        height: 50,
                       ),
-                      SizedBox(
-                        height: 10,
+
+                      /// #fields
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          elevation: 10,
+                          child: Column(
+                            children: const [
+
+                              // #fullname
+                              TextField(
+                                decoration: InputDecoration(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    hintText: "Fullname",
+                                    border: InputBorder.none),
+                              ),
+                              Divider(),
+
+                              // #email
+                              TextField(
+                                decoration: InputDecoration(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    hintText: "Email",
+                                    border: InputBorder.none),
+                              ),
+                              Divider(),
+
+                              // #phone
+                              TextField(
+                                decoration: InputDecoration(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    hintText: "Phone",
+                                    border: InputBorder.none),
+                              ),
+                              Divider(),
+
+                              // #password
+                              TextField(
+                                decoration: InputDecoration(
+                                    contentPadding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    hintText: "Password",
+                                    border: InputBorder.none),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        "Welcome",
-                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      const SizedBox(
+                        height: 30,
                       ),
+
+                      /// #signUp button
+                      Container(
+                        height: 50,
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade700,
+                            borderRadius: BorderRadius.circular(25)),
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          )),
+                          onPressed: () {},
+                          child: const Text(
+                            "Sign Up",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        "Sign Up with SNS",
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+
+                      // #SNS facebook, google, apple
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.blue,
+                                minimumSize: const Size(110, 50),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "Facebook",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.red,
+                                minimumSize: const Size(110, 50),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "Google",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                backgroundColor: Colors.black,
+                                minimumSize: const Size(110, 50),
+                              ),
+                              onPressed: () {},
+                              child: const Text(
+                                "Apple",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
               ),
-            ),
-            
-            /// #body
-            Expanded(
-              flex: 3,
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40))),
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    
-                    /// #fields
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        elevation: 10,
-                        child: Column(
-                          children: const [
-
-                            // #fullname
-                            TextField(
-                              decoration: InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(horizontal: 10),
-                                  hintText: "Fullname",
-                                  border: InputBorder.none),
-                            ),
-                            Divider(),
-
-                            // #email
-                            TextField(
-                              decoration: InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(horizontal: 10),
-                                  hintText: "Email",
-                                  border: InputBorder.none),
-                            ),
-                            Divider(),
-
-                            // #phone
-                            TextField(
-                              decoration: InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(horizontal: 10),
-                                  hintText: "Phone",
-                                  border: InputBorder.none),
-                            ),
-                            Divider(),
-
-                            // #password
-                            TextField(
-                              decoration: InputDecoration(
-                                  contentPadding:
-                                      EdgeInsets.symmetric(horizontal: 10),
-                                  hintText: "Password",
-                                  border: InputBorder.none),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-
-                    /// #signUp button
-                    Container(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade700,
-                          borderRadius: BorderRadius.circular(25)),
-                      child: TextButton(
-                        style: TextButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        )),
-                        onPressed: () {},
-                        child: const Text(
-                          "Sign Up",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      "Sign Up with SNS",
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-
-                    // #SNS facebook, google, apple
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.blue,
-                              minimumSize: const Size(110, 50),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              "Facebook",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.red,
-                              minimumSize: const Size(110, 50),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              "Google",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              backgroundColor: Colors.black,
-                              minimumSize: const Size(110, 50),
-                            ),
-                            onPressed: () {},
-                            child: const Text(
-                              "Apple",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
