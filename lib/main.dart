d2bf7804240f361b3c65ea78a2e7bffc50cd09e6
Miracle-Blue <merlin.com8.ar@gmail.com>
2022-01-17 22:35:04 +0500

    SignUp UI

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:signup_ui/pages/sign_up_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignUpPage(),
+    );
+  }
+}
